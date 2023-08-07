7ff1ee17ff9198904fa807081a806a60dcd4f62e
lordelogos <paulehiks@gmail.com>
2023-08-07 12:17:57 +0100

    fix: base url patch, setup config for script usage

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 import { version } from "../../package.json";
 
-export const API_BASE_URL = "https://api.clientmail.xyz/";
+export const API_BASE_URL = "https://api.clientmail.xyz";
 export const USER_AGENT = `client-mail:${version}`;
